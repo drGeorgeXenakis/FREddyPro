b7c0cf55b609a7308b1e5bd83f6161fd0d02769d
drGeorgeXenakis <george.xenakis@gmail.com>
2022-03-04 13:14:53 +0000

    Deleting files from version 1.0 not used anymore

--- R/cleanSecondVar.r
+++
@@ -1,7 +0,0 @@
-cleanSecondVar <-
-function (x, y, data) 
-{
-    clean <- which(is.na(data[[x]]) & !is.na(data[[y]]))
-    data[[y]][clean] <- NA
-    return(data)
-}
--- R/cleanVar.r
+++
@@ -1,7 +0,0 @@
-cleanVar <-
-function (x, data, lessThan = NULL, greaterThan = NULL) 
-{
-    clean <- which(data[[x]] >= greaterThan | data[[x]] <= lessThan)
-    data[[x]][clean] <- NA
-    return(data)
-}
